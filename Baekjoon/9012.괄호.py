7261cc825004dc3eddfa23794ea441f696ba5667
신승혁 <Cozak92@gmail.com>
2021-05-03 17:01:20 +0900

    2021/05/03

---
+++ Baekjoon/9012.괄호.py
@@ -0,0 +1,25 @@
+t = int(input())
+
+
+
+for _ in range(t):
+    stack = []
+
+    braket = input()
+    flag = True
+    for b in braket:
+
+        if b == "(":
+            stack.append(b)
+        else:
+            if not stack:
+                print("NO")
+                flag = False
+                break
+            else:
+                stack.pop()
+        print(stack)
+    if stack:
+        print("NO")
+        flag = False
+    if flag : print("YES")
