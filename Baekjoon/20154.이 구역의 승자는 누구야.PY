be667452b03c19936c186d63e205d601da8950dd
신승혁 <Cozak92@gmail.com>
2021-05-03 18:02:01 +0900

    20201/05/03

---
+++ Baekjoon/20154.이 구역의 승자는 누구야.PY
@@ -0,0 +1,9 @@
+alpha='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers='32123333113133122212112221'
+cnt=0
+s=input()
+for i in range(len(s)):
+   cnt+=int (numbers[alpha.index(s[i])])
+   cnt%=10
+if cnt%2==0: print("You're the winner?")
+else: print("I'm a winner!")
