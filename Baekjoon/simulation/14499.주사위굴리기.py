90f11832687b4986c0057206cc760be486110b01
Cozak92 <Cozak92@gmail.com>
2021-05-06 00:18:00 +0900

    2021/05/06

--- Baekjoon/simulation/14499.주사위굴리기.py
+++ Baekjoon/simulation/14499.주사위굴리기.py
@@ -1,90 +1,57 @@
-from collections import deque
-
-north = [0,0,0,0]
-east = [0,0,0]
-# north index 2 = east index 2
-# if move to south - 
-# t = north.pop()
-# north.appendleft(t)
-# east[1] = north[1]
-# north[-1] // bottom
-# north[0] // top
-
-# if move to north
-# t = north.popleft()
-# north.append(t)
-# east[1] = north[1]
-# north[0] // bottom
-# north[-1] // top
-north = deque([0,0,0,0])
-east = deque([0,0,0])
-
 n, m, x, y, k = list(map(int, input().split()))
 board = [list(map(int, input().split())) for _ in range(n)]
+dice = [0,0,0,0,0,0]
+
+DIR = [[4,2,1,6,5,3],[3,2,6,1,5,4],[2,6,3,4,1,5],[5,1,3,4,6,2]]
+
+def doRotate(order):
+    global DIR
+    temp = [0 for _ in range(6)]
+
+    for i in range(6):
+
+        temp[DIR[order][i] - 1] = dice[i]
+
+    for i in range(6):
+
+        dice[i] = temp[i]
+
+
+
 order = list(map(int, input().split()))
+
 print(order)
 for o in order:
-    print(north,east)
-    if o == 1:
-        if y + 1 >= m:
-            continue
-        print(east[-1])
-        y += 1
-        t = east.popleft()
-        east.append(t)
-        north[1] = east[1] 
-        if board[x][y] == 0:
-            board[x][y] = east[1] 
-        else:
-            east[1] = board[x][y]
-            board[x][y] = 0
-        
-        
-            
-    if o == 2:
-        if y - 1 < 0:
-            continue
-        y -= 1
-        print(east[0])
-        t = east.pop()
-        east.appendleft(t)
-        north[1] = east[1] 
-        if board[x][y] == 0:
-            board[x][y] = east[1] 
-        else:
-            east[1] = board[x][y]
-            board[x][y] = 0
-        
-        
-    if o == 3:
-        if x - 1 < 0:
-            continue
-        print(north[-2])
-        x -= 1
-        t = north.popleft()
-        north.append(t)
-        east[1] = north[1]
-        if board[x][y] == 0:
-            board[x][y] = north[1] 
-        else:
-            north[1] = board[x][y]
-            board[x][y] = 0
-        
-        
+    #print(x,y)
 
-    if o == 4:
-        if x + 1 >= n:
-            continue
-        print(north[0])
+    flag = True
+
+    if(y < m - 1 and o == 1):
+        doRotate(o - 1)
+        y += 1
+    elif(y > 0 and o == 2):
+        doRotate(o - 1)
+        y -= 1
+    elif(x > 0 and o == 3):
+        doRotate(o - 1)
+        x -= 1
+    elif(x < n - 1 and o == 4):
+        doRotate(o - 1)
         x += 1
-        t = north.pop()
-        north.appendleft(t)
-        
+    else: flag = False
+
+
+    if flag:
         if board[x][y] == 0:
-            board[x][y] = north[1] 
+            board[x][y ]= dice[5]
         else:
-            north[1] = board[x][y]
+            dice[5] = board[x][y]
             board[x][y] = 0
-        east[1] = north[1]
-        
+
+        print(dice[0])
+
+
+    
+    
+
     
