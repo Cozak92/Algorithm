9485a3e76fc244453555aee5a5dbfdb47229a7b4
신승혁 <Cozak92@gmail.com>
2021-05-13 09:16:45 +0900

    2021/05/13

--- Baekjoon/Simulation/16234.인구이동.py
+++ Baekjoon/Simulation/16234.인구이동.py
@@ -1,62 +1,56 @@
 from collections import deque
 dx = [0,0,1,-1]
 dy = [1,-1,0,0]
+def bfs():
+    q = deque()
+    isVisited = [[False for _ in range(n)] for __ in range(n)]
+    flag = False
+    for i in range(n):
+        for j in range(n):        
+            if not isVisited[i][j]:
+                sum = nations[i][j]
+                stack = []
+                q.append((i,j))
+                stack.append((i,j))
+                isVisited[i][j] = True
+
+                while q:
+                    x,y = q.popleft()
+
+                    for k in range(4):
+                        nx = x + dx[k]
+                        ny = y + dy[k]
+
+                        if 0 <= nx < n and 0 <= ny < n and not isVisited[nx][ny]:
+                            
+                            if l <= abs(nations[x][y] - nations[nx][ny]) <= r:
+
+                                sum += nations[nx][ny]
+                                isVisited[nx][ny] = True
+                                stack.append((nx,ny))
+                                q.append((nx,ny))
+
+                if len(stack) >= 2:
+                    avg = sum // len(stack)
+                    flag= True
+                    for a,b in stack:
+                        nations[a][b] = avg
+
+
+    return flag
 
 n, l, r = list(map(int,input().split()))
 
 nations = [list(map(int, input().split())) for _ in range(n)]
 isOpen = [[False for _ in range(n)] for __ in range(n)]
 
-q = deque()
+
 ans = 0
-
-for i in range(n):
-    for j in range(n):
-        isOpen = [[False for _ in range(n)] for __ in range(n)]
-        isVisited = [[False for _ in range(n)] for __ in range(n)]
-        if not isVisited[i][j]:
-            for f in nations:
-                print(f)
-            q.append((i,j))
-            isVisited[i][j] = True
-            minNum = nations[i][j]
-
-            while q:
-                x,y = q.popleft()
-
-                for k in range(4):
-                    nx = x + dx[k]
-                    ny = y + dy[k]
-
-                    if 0 <= nx < n and 0 <= ny < n and not isVisited[nx][ny]:
-                        
-                        if l <= abs(minNum - nations[nx][ny]) <= r:
-                            minNum = min(minNum,nations[nx][ny])
-                            isOpen[x][y] = True                           
-                            isOpen[nx][ny] = True
-
-                            isVisited[nx][ny] = True
-                            
-                            q.append((nx,ny))
-
-            SUM = 0
-            cnt = 0
-            flag = False
-            for a in range(n):
-                for b in range(n):
-                    if isOpen[a][b]:
-                        print(a,b)
-                        SUM += nations[a][b]
-                        cnt += 1
-                        flag = True
-            for a in range(n):
-                for b in range(n):
-                    if isOpen[a][b]:
-                        nations[a][b] = SUM // cnt
-
-            if flag: ans += 1
-                
-            
+while True:
+    flag = bfs()
+    if flag:
+        ans += 1
+    else: break
 print(ans)
             
 
