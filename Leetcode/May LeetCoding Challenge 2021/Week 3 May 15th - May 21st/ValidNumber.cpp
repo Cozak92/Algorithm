2b585efbc53f4e12b37b308959c91316cf54f03f
신승혁 <Cozak92@gmail.com>
2021-05-17 16:54:52 +0900

    2021/05/17

---
+++ Leetcode/May LeetCoding Challenge 2021/Week 3 May 15th - May 21st/ValidNumber.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = 0;
+    // 그리디하게 접근
+    // 항상 부모 노드에 카메라를 설치하는게 이득이다.
+    // 0 == leaf 노드
+    // 1 == parent 노드
+    // 2 == 이미 카메라 설치됨
+    int minCameraCover(TreeNode* root) {
+        return (dfs(root) < 1 ? 1 : 0) + res;
+    }
+
+    int dfs(TreeNode* root) {
+        if (!root)
+            return 2; // 기저조건 nullptr
+        int left = dfs(root->left), right = dfs(root->right);
+        if (left == 0 || right == 0) {
+            res++;
+            return 1;
+        }
+        return left == 1 || right == 1 ? 2 : 0;
+    }
+};
--- test.cpp
+++ test.cpp
@@ -1,94 +1,26 @@
-#include<iostream>
- 
-#define endl "\n"
-#define MAX 9
+// #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
- 
-int MAP[MAX][MAX];
-bool Row[MAX][MAX];
-bool Col[MAX][MAX];
-bool Square[MAX][MAX];
- 
-void Input()
-{
-    for (int i = 0; i < MAX; i++)
-    {
-        for (int j = 0; j < MAX; j++)
-        {
-            cin >> MAP[i][j];
-            if (MAP[i][j] != 0)
-            {
-                Row[i][MAP[i][j]] = true;
-                Col[j][MAP[i][j]] = true;
-                Square[(i / 3) * 3 + (j / 3)][MAP[i][j]] = true;
-            }
-        }
-    }
-}
- 
-void Print()
-{
-    for (int i = 0; i < MAX; i++)
-    {
-        for (int j = 0; j < MAX; j++)
-        {
-            cout << MAP[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
- 
-void DFS(int Cnt)
-{
-    int x = Cnt / MAX;    // x 좌표
-    int y = Cnt % MAX;    // y 좌표
-    
-    if (Cnt == 81)
-    {
-        Print();
-        exit(0);
-    }
- 
-    if (MAP[x][y] == 0)
-    {
-        for (int i = 1; i <= 9; i++)
-        {
-            if (Row[x][i] == false && Col[y][i] == false && Square[(x / 3) * 3 + (y / 3)][i] == false)
-            {
-                Row[x][i] = true;
-                Col[y][i] = true;
-                Square[(x / 3) * 3 + (y / 3)][i] = true;
-                MAP[x][y] = i;
-                DFS(Cnt + 1);
-                MAP[x][y] = 0;
-                Row[x][i] = false;
-                Col[y][i] = false;
-                Square[(x / 3) * 3 + (y / 3)][i] = false;
-            }
-        }
-    }
-    else DFS(Cnt + 1);
-}
- 
-void Solution()
-{
-    DFS(0);
-}
- 
-void Solve()
-{
-    Input();
-    Solution();
-}
- 
-int main(void)
-{
+#define FOR(i,m,n) for(int i=(m);i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(),(v).end()
+using ll = long long;
+// constexpr int INF = 0x3f3f3f3f;
+// constexpr long long LINF = 0x3f3f3f3f3f3f3f3fLL;
+// constexpr double EPS = 1e-8;
+// constexpr int MOD = 1000000007;
+// // constexpr int MOD = 998244353;
+// constexpr int dy[] = {1, 0, -1, 0}, dx[] = {0, -1, 0, 1};
+// constexpr int dy8[] = {1, 1, 0, -1, -1, -1, 0, 1}, dx8[] = {0, -1, -1, -1, 0, 1, 1, 1};
+
+
+int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
- 
-    //freopen("Input.txt", "r", stdin);
-    Solve();
- 
-    return 0;
+
+    int n,m;
+
+    cin >> n >> m;
+    cout << n << m;
 }
