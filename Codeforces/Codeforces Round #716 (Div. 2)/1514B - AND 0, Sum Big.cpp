bd5d32f8a3d2ce7871a1cb242aea77283c175d43
신승혁 <Cozak92@gmail.com>
2021-05-26 18:04:26 +0900

    2021/05/26

---
+++ Codeforces/Codeforces Round #716 (Div. 2)/1514B - AND 0, Sum Big.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,m,n) for(int i=(m);i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(),(v).end()
+#define what_is(x) cerr << #x << " is " << x << "\n"
+using ll = long long;
+using pii = pair<int,int>;
+constexpr int INF = 0x3f3f3f3f;
+constexpr long long LINF = 0x3f3f3f3f3f3f3f3fLL;
+constexpr double EPS = 1e-8;
+constexpr int MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr int dy[] = {1, 0, -1, 0}, dx[] = {0, -1, 0, 1};
+constexpr int dy8[] = {1, 1, 0, -1, -1, -1, 0, 1}, dx8[] = {0, -1, -1, -1, 0, 1, 1, 1};
+const int MX = 1;
+
+void solve(){
+    int n,k;
+    cin >> n >> k;
+    ll ans = 1;
+
+    for(int i = 0; i < k; i++) ans = (ans * n) % MOD;
+    cout << ans << '\n';
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; cin >> t;
+    while(t--) solve();
+}
--- test.cpp
+++ test.cpp
@@ -5,33 +5,7 @@ using namespace std;
 
 int main() {
 	int n, k = 0;
-	cin >> n >> k;
-	int cnt = 0;
-	string s;
 
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j < 60; j++) {
-			for (int l = 0; l < 60; l++) {
-				if (i / 10 == 0) {
-					s += "0";
-				}
-				s += to_string(i);
-				if (j / 10 == 0) {
-					s += "0";
-				}
-				s += to_string(j);
-				if (l / 10 == 0) {
-					s += "0";
-				}
-				s += to_string(l);
-				if (s.find(to_string(k)) != -1) {
-					cnt++;
-				}
-				s.clear();
-			}
-		}
-	}
-	std::cout << cnt;
-
+	cout << (1 << 1) << "\n";
 	return 0;
 }
