040bfa29ea53726dec30205fdcf1acfdffe19aae
신승혁 <Cozak92@gmail.com>
2021-09-01 19:58:10 +0900

    1325.효율적인 해킹

--- Baekjoon/BFSAndDFS/Baekjoon1325.java
+++ Baekjoon/BFSAndDFS/Baekjoon1325.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 
 
-public class Algoritm1325 {
+public class Baekjoon1325 {
   static int stoi(String s) { return Integer.parseInt(s);}
   static final int INF = 987654321;
 	static int N,M;
@@ -22,6 +22,7 @@ public class Algoritm1325 {
 			int cur = q.poll();
 			
 			for(int next : arr.get(cur) ){
+
 				if(isVisited[next] != true){
 					isVisited[next] = true;
 					q.add(next);
@@ -39,43 +40,41 @@ public class Algoritm1325 {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //선언
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));//선언
     StringTokenizer st = new StringTokenizer(br.readLine());
-		N = stoi(st.nextToken());
-		M = stoi(st.nextToken());
-		List<List<Integer>> arr = new ArrayList<List<Integer>>(N);
+	N = stoi(st.nextToken());
+	M = stoi(st.nextToken());
+	List<List<Integer>> arr = new ArrayList<List<Integer>>(N);
 
 
-		for(int i = 0; i <=N; i++){
-			arr.add(new ArrayList<>());
+	for(int i = 0; i <=N; i++){
+		arr.add(new ArrayList<>());
+	}
+	for(int i = 0; i < M; i++){
+		
+		st = new StringTokenizer(br.readLine());
+		int a = stoi(st.nextToken());
+		int b = stoi(st.nextToken());
+
+		
+		arr.get(b).add(a);
+	}
+
+	int ret = Integer.MIN_VALUE;
+	int[] cnt = new int[N+1];
+
+	for(int i = 1; i <= N; i++){
+		int temp = bfs(i,arr);
+		ret = Math.max(ret,temp);
+		cnt[i] = temp;
+	}
+
+	for(int i = 1; i <= N; i++){
+		if(cnt[i] == ret){
+			bw.write(i + " ");
 		}
-		for(int i = 0; i < M; i++){
-			st = new StringTokenizer(br.readLine());
-			int a = stoi(st.nextToken());
-			int b = stoi(st.nextToken());
-
-			
-			arr.get(b).add(a);
-		}
-
-		int ret = -1;
-		int[] cnt = new int[N+1];
-
-		for(int i = 1; i <= N; i++){
-			int temp = bfs(i,arr);
-			ret = (temp > ret) ? temp : ret;
-			cnt[i] += 1;
-		}
-
-		for(int c : cnt){
-			System.out.print(c);
-			if(c == ret){
-				System.out.print(c + " ");
-			}
-		}
+	}
 
     bw.flush();
     bw.close();
     br.close();
-
-    System.out.println("Hello World");
   }
 }
