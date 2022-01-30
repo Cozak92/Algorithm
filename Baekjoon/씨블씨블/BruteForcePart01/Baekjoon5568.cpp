ac103aae80f2c28b73fb95f394913a91f7f9e59b
신승혁 <Cozak92@gmail.com>
2022-01-30 11:22:50 +0900

    알고리즘 스터디 1주차 2문제

---
+++ Baekjoon/씨블씨블/BruteForcePart01/Baekjoon5568.cpp
@@ -0,0 +1,52 @@
+//
+// Created by cozak on 2022-01-30.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,m,n) for(int i=(m);i<(n);i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(),(v).end()
+#define debug(x) cerr << #x << " is " << x << "\n"
+using ll = long long;
+using pii = pair<int,int>;
+constexpr int INF = 0x3f3f3f3f;
+constexpr long long LINF = 0x3f3f3f3f3f3f3f3fLL;
+constexpr double EPS = 1e-8;
+constexpr int MOD = 1000000007;
+// constexpr int MOD = 998244353;
+constexpr int dy[] = {1, 0, -1, 0}, dx[] = {0, -1, 0, 1};
+constexpr int dy8[] = {1, 1, 0, -1, -1, -1, 0, 1}, dx8[] = {0, -1, -1, -1, 0, 1, 1, 1};
+const int MX = 10;
+int n,k,ans;
+bool isVisited[15];
+
+void dfs(int idx, int step){
+
+    if( step == k){
+        ans++;
+        return;
+    }
+
+    FOR(i,idx,n+1){
+        if(isVisited[i]) continue;
+
+        isVisited[i] = true;
+        dfs(i+1,step+1);
+        isVisited[i] = false;
+    }
+}
+
+
+void solve(){
+    cin >> n >> k;
+    dfs(0,0);
+    cout << ans;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    solve();
+}
