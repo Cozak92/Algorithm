406aeeb9b95fe3e354c05aa7a26788ce66c24ca8
신승혁 <cozak@sellmate.co.kr>
2022-03-29 13:29:29 +0900

    5주차 1문제

--- Baekjoon/사내스터디/5주차/Baekjoon2630.kt
+++ Baekjoon/사내스터디/5주차/Baekjoon2630.kt
@@ -5,7 +5,10 @@ import java.util.*
 object Baekjoon2630 {
 
     fun recursive(k: Int){
-
+        // 왼쪽 0,0 ~ k/2, k/2
+        // 오른쪽위 0,k/2 ~ k/2, k
+        // 왼쪽아래 k/2,0 ~ k,k/2
+        // 오른쪽아래 k/2,k/2 ~ k,k
 
 
     }
